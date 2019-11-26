3b5b6d812110fedcf16f38446ecae6f5f36f8ee7
WhatIsOO <chownema@gmail.com>
2019-11-26 18:16:11 +1300

    F21-Add-support-for-saving-and-getting-Pricing-rules-for-user-logged-in-company-models
    fixed made priceRuleSet public

--- src/model/domain/quote/priceRuleLocationConditional.ts
+++ src/model/domain/quote/priceRuleLocationConditional.ts
@@ -1,6 +1,14 @@
 // Used before selecting a price rule set
 import { PriceRuleSet } from './priceRuleSet';
+import { PriceRule } from './priceRule';
 
 export class PriceRuleLocationConditional {
-    constructor(public locationList?: Array<string>, priceRuleSet?: PriceRuleSet){ }
+    constructor(public locationList?: Array<string>, public priceRuleSet?: PriceRuleSet){ }
+
+    public getlocationalconditionalstep(): PriceRule {
+        const prList = this.priceRuleSet ? this.priceRuleSet.priceRules : [];
+        return prList.filter((pr) => {
+            return pr.stepNo === -1;
+        })[0]; // get first item
+    }
 }
