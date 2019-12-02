b8e56917fa4299c0681a8d0fce4d88847d0285c7
nphumy98 <38494628+nphumy98@users.noreply.github.com>
2019-12-02 22:34:24 +1300

    [F13]: remove setter method of ErrorResponse

--- src/model/utils/errorResponse.ts
+++ src/model/utils/errorResponse.ts
@@ -2,8 +2,4 @@ import { MessageField } from "./messageField";
 
 export class ErrorResponse {
     constructor(public error?: string, public message?: string, public detail?: Array<MessageField>){};
-
-    public setDetail(aDetail: Array<MessageField>): void {
-        this.detail = aDetail;
-    }
 }
