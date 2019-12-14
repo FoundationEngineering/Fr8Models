9492736c1576639a0c6bceddd2c79d8e2aa7dfc6
WhatIsOO <chownema@gmail.com>
2019-12-14 13:02:15 +1300

    FM25-Add-Quote-Id-to-the-quote-model
    Added uuid func
    Added quote id to quote model

--- src/model/domain/quote/quote.ts
+++ src/model/domain/quote/quote.ts
@@ -1,6 +1,7 @@
 export class Quote {
-    constructor(public id?: number, public iCOSRef?: number, public companyId?: number,
-                public companyName?: string, public createdAt?: Date, public updatedAt?: Date,
-                public weight?: number, public mass?: number, public cost?: number, public to?: string,
-                public from?: string, public parentCompanyNote?: string, public parentCompanyId?: number) { }
+    constructor(public id?: number, public iCOSRef?: number, public quoteId?: string,
+        public companyId?: number, public companyName?: string, public createdAt?: Date,
+        public updatedAt?: Date, public weight?: number, public mass?: number,
+        public cost?: number, public to?: string, public from?: string, public parentCompanyNote?: string, 
+        public parentCompanyId?: number) { }
 }
