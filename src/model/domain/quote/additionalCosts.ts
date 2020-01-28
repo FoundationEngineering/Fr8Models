1f4ad37fe1f83babf33fd602226deafde8ed7896
WhatIsOO <chownema@gmail.com>
2020-01-29 12:19:34 +1300

    Added aditional costs as a list and updated the package name as fr8-models

--- src/model/domain/quote/additionalCosts.ts
+++ src/model/domain/quote/additionalCosts.ts
@@ -1,4 +1,11 @@
 export class AdditionalCost {
+    public static TYPE: any = {
+        DRIVER_COST: 'type_driver_cost',
+        UPLIFT_COST: 'type_uplift_cost',
+        FUEL_ADJUST_FEE: 'type_fuel_adjust_fee',
+        PALLET_COST: 'type_pallet_cost',
+    };
+
     constructor(public name?: string, public cost?: number) {
         this.cost = cost ? cost : 0.00;
     }
