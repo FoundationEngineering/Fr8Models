20876076f4974ee7b303fc26f2d0050ff0aa8691
WhatIsOO <chownema@gmail.com>
2020-02-19 23:40:44 +1300

    Added ratingcardTonneVolumeRatesData model

--- src/model/domain/quote/ratingCard/ratingCardTonneVolumeRates.ts
+++ src/model/domain/quote/ratingCard/ratingCardTonneVolumeRates.ts
@@ -1,8 +1,19 @@
-import { PricingRateLevelPair } from '../pricingRateLevel';
+import { PricingRateLevelPair, PricingRateLevel } from '../pricingRateLevel';
+
+export class RatingCardTonneVolumeRatesData {
+    constructor(
+        public ratingCardName: String,
+        public rates: PricingRateLevelPair[]
+        ) {}
+}
 
 export class RatingCardTonneVolumeRates {
-    constructor(public id?: number, public parentCompanyId?: number,
-        public companyId?: number, public companyName?: string, public createdAt?: Date, public updatedAt?: Date,
-        public data?: PricingRateLevelPair[]) { }
+    constructor(public id?: number,
+        public parentCompanyId?: number,
+        public companyId?: number,
+        public companyName?: string,
+        public createdAt?: Date,
+        public updatedAt?: Date,
+        public data?: RatingCardTonneVolumeRatesData) { }
 
 }
