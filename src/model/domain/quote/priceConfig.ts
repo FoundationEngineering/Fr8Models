e831967e37c43fe2e7bd965a5aa026e932fccda9
WhatIsOO <chownema@gmail.com>
2020-02-20 11:18:24 +1300

    FEAT: added custom rating card to the prIce config model

--- src/model/domain/quote/priceConfig.ts
+++ src/model/domain/quote/priceConfig.ts
@@ -1,11 +1,13 @@
 import { RatingCard } from './ratingCard/ratingCard';
 import { PricingRateLevel } from './pricingRateLevel';
+import { RatingCardTonneVolumeRates } from './ratingCard/ratingCardTonneVolumeRates';
 
 export class PriceConfig {
     constructor(public ratingCard: RatingCard,
                 public tonneRate?: number, public massRate?: number,
                 public palletRate?: number, public gstRate?: number,
                 public flatCost?: number, public fafRate?: number,
-                public minCharge?: number
+                public minCharge?: number,
+                public ratingCardTonneVolumeRatesList?: RatingCardTonneVolumeRates[]
                ) { }
 }
