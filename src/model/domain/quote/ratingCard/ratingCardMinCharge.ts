c0349999cde0f499d021cdb419e9e957c80451ce
WhatIsOO <chownema@gmail.com>
2020-02-24 13:51:30 +1300

    FEAT: Added rating card min charge model

---
+++ src/model/domain/quote/ratingCard/ratingCardMinCharge.ts
@@ -0,0 +1,22 @@
+export class MinChargeData {
+
+    constructor(
+        public toLocation: string,
+        public cost: number) {}
+}
+
+export class RatingCardMinChargeData {
+    constructor(
+        public rates: MinChargeData[]
+        ) {}
+}
+
+export class RatingCardMinCharge {
+    constructor(public id?: number,
+        public parentCompanyId?: number,
+        public companyId?: number,
+        public companyName?: string,
+        public createdAt?: Date,
+        public updatedAt?: Date,
+        public data?: RatingCardMinChargeData) { }
+}
