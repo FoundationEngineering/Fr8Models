754710570dfb3234f21ee3f542240c1f5f6ead8e
WhatIsOO <chownema@gmail.com>
2020-02-28 18:31:39 +1300

    REF:  refactored constructor to more readable standard

--- src/model/domain/quote/quoteInterface/priceConfig.ts
+++ src/model/domain/quote/quoteInterface/priceConfig.ts
@@ -1,14 +1,19 @@
 import { RatingCard } from '../ratingCard/ratingCard';
 import { RatingCardTonneVolumeRates } from '../ratingCard/ratingCardTonneVolumeRates';
 import { RatingCardMinCharge } from '../ratingCard/ratingCardMinCharge';
+import { RatingCardPalletRate } from '../ratingCard/ratingCardPalletRate';
 
 export class PriceConfig {
     constructor(public ratingCard: RatingCard,
-                public tonneRate?: number, public massRate?: number,
-                public palletRate?: number, public gstRate?: number,
-                public flatCost?: number, public fafRate?: number,
+                public tonneRate?: number,
+                public massRate?: number,
+                public palletRate?: number,
+                public gstRate?: number,
+                public flatCost?: number,
+                public fafRate?: number,
                 public minCharge?: number,
                 public ratingCardTonneVolumeRatesList?: RatingCardTonneVolumeRates[],
-                public ratingCardMinChargeList?: RatingCardMinCharge[]
+                public ratingCardMinChargeList?: RatingCardMinCharge[],
+                public ratingCardPalletRateList?: RatingCardPalletRate[]
                ) { }
 }
--- src/model/domain/quote/quoteInterface/quoteInterfaceRequest.ts
+++ src/model/domain/quote/quoteInterface/quoteInterfaceRequest.ts
@@ -2,14 +2,17 @@ export class QuoteInterfaceRequest {
     public tonne: number;
     public totalCalculatedCost: number;
     public totalCost: number;
-    constructor(public to: string = '', public from: string = '',
-                public weight: number = 0, public mass: number = 0, 
-                public palletAmount: number = 0,
-                public driverCost: number = 0,
-                public upliftCost: number = 0,
-               ) {
-                   this.totalCalculatedCost = 0;
-                   this.totalCost  =  0;
-                   this.tonne = this.weight * 0.001;
-               }
+    constructor(
+        public to: string = '',
+        public from: string = '',
+        public weight: number = 0,
+        public mass: number = 0,
+        public palletAmount: number = 0,
+        public driverCost: number = 0,
+        public upliftCost: number = 0,
+    ) {
+        this.totalCalculatedCost = 0;
+        this.totalCost = 0;
+        this.tonne = this.weight * 0.001;
+    }
 }
