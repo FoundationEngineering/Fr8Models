aa7ed6cb9261b9f40d76f11d16ad83d445656a61
WhatIsOO <chownema@gmail.com>
2020-02-28 17:53:48 +1300

    FEAT: Added palletno

--- src/model/domain/quote/quote.ts
+++ src/model/domain/quote/quote.ts
@@ -1,9 +1,21 @@
 import { AdditionalCost } from './additionalCosts';
 
 export class Quote {
-    constructor(public id?: number, public iCOSRef?: number, public quoteId?: string,
-        public companyId?: number, public companyName?: string, public createdAt?: Date,
-        public updatedAt?: Date, public weight?: number, public mass?: number,
-        public cost?: number, public to?: string, public from?: string, public parentCompanyNote?: string, 
-        public parentCompanyId?: number, public additionalCosts?: AdditionalCost[]) { }
+    constructor(public id?: number,
+        public iCOSRef?: number,
+        public quoteId?: string,
+        public companyId?: number,
+        public companyName?: string,
+        public createdAt?: Date,
+        public updatedAt?: Date,
+        public weight?: number,
+        public mass?: number,
+        public cost?: number,
+        public to?: string,
+        public from?: string,
+        public parentCompanyNote?: string, 
+        public parentCompanyId?: number,
+        public additionalCosts?: AdditionalCost[],
+        public palletNo?: number
+        ) { }
 }
