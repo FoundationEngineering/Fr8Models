f4f2a04b77832a85e1b0c399a55a946b1f9c0429
WhatIsOO <chownema@gmail.com>
2020-03-18 20:52:38 +1300

    FEAT: added message to quoteinterface response
    this allows for debugger work
    at the level of the component level
    this helps find out how did cost come to be from the quote interface

--- src/model/domain/quote/quoteInterface/quoteInterfaceResponse.ts
+++ src/model/domain/quote/quoteInterface/quoteInterfaceResponse.ts
@@ -1,4 +1,4 @@
 export class QuoteInterfaceResponse {
-    constructor(public success: boolean, public cost: number) {}
+    constructor(public success: boolean, public cost: number, public message?: any) {}
 }
 
