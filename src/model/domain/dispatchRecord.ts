3d6798e9e9ea54b38ad1532b21ae9af491bb5f50
WhatIsOO <chownema@gmail.com>
2020-04-01 22:47:25 +1300

    FEAT:  added changes to the dispatch record to match the dto in the backend

--- src/model/domain/dispatchRecord.ts
+++ src/model/domain/dispatchRecord.ts
@@ -1,108 +1,114 @@
 export class DispatchRecord {
 // Use data mapper layer translate the backend
-    public userId: number;
-    public companyId: number;
-    public parentCompanyId: number;
+constructor(
+    public id?: number,
+    public userId?: number,
+    public companyId?: number,
+    public parentCompanyId?: number,
+    public manifestId?: number,
+    public createdDate?: Date,
+    public updatedDate?: Date,
 
-    public id: string;
-    public flags: string;
-    public jobType: string;
-    public docketId: string;
-    public orderRefId: string;
-    public deliveryRefId: string;
-    public pickupRefId: string; // TODO: not in back
-    public iCOSRef: number;
-    public exportCertNumber: string;
+    public historyData?: string,
 
-    public isNoDisp: boolean;
-    public isQuote: boolean;
+    public docket?: string,
+    public orderRef?: string,
+    public deliveryRef?: string,
+    public pickupRef?: string,
+    public iCOSRef?: number,
+    
+    public flags?: string,
+    public jobType?: string,
+    public comodity?: string,
 
-    public shipPayer: string; // TODO: not in back
+    public bookedBy?: string,
 
-    public shipperName: string;
-    public shipperAddr: string;
+    public exportCertNumber?: string,
+    
+    public customer?: string,
 
-    public consigneeName: string;
-    public consigneeAddr: {[key: string]: string}; // NOTE: multi line (4 lines) address
+    public consigneeId?: number,
+    public consigneeName?: string,
+    public pickupAddr?: string,
+    public pickupAddrLocality?: string,
+    public deliverAddr?: string,
+    public deliverAddrLocality?: string,
 
-    public createdDate: string; // TODO: best to be a real date obj
-    public status: string; // TODO: best to be a real enum obj
-    public serviceType: string; // TODO: best to be a real enum obj
+    public status?: string,
+    public serviceType?: string,
 
-    public hirePallets: number;
-    public nonHirePallets: number;
+    public palletsHire?: number,
+    public nonHirePallets?: number,
 
-    public lifts: number;
-    public hireRetn: number;
-    public goodsType: number;
+    public lifts?: number,
+    public goodsType?: number,
+    // Name
+    public goods?: string,
+    public commodity?: string,
+    
+    public reqTempDeg?: number,
+    public actualTempDeg?: number,
 
-    public reqTempDeg: number;
-    public actualTempDeg: number;
+    public unloadFee?: number,
 
-    public unloadFee: number;
-    public noChg: boolean;
-    public noDisp: boolean;
+    public bookedDateTime?: Date,
+    public reqPickupDateTime?: Date,
+    public actPickupDateTime?: Date,
 
-    public reqPickupDateTime: string; // TODO: best to be a real date obj
-    public actPickupDateTime: string; // TODO: best to be a real date obj
+    public reqDeliveryDateTime?: Date,
+    public actDeliveryDateTime?: Date,
 
-    public reqDeliveryTime: string;
-    public actDeliveryTime: string;
+    public pUPTZ?: string,
+    public dELTZ?: string,
 
-    public deliveredTo: string;
-    public hasPOD: boolean;
-    public comments: string; // TODO: set limit 255
+    public quantity?: number,
+    public weight?: number,
+    public cubic?: number,
 
-    public nextDepot: string; // TODO: load from db
-    public deliveryDepot: string; // TODO: load from db
-    public currentDepot: string; // TODO: load from db
-    constructor(args: any) {
-        this.id = '';
-        this.flags = '';
-        this.jobType = '';
-        this.docketId = '';
-        this.orderRefId = '';
-        this.deliveryRefId = '';
-        this.pickupRefId = ''; // TODO= not in back
-        this.iCOSRef = 0;
-        this.exportCertNumber = '';
-        this.isNoDisp = false;
-        this.isQuote = false;
-        this.pickupRefId = ''; // TODO= not in back
-        this.shipperName = '';
-        this.shipperAddr = '';
-        this.consigneeName = '';
-        this.consigneeAddr = {}; // NOTE= multi line (4 lines) address
-        this.createdDate = ''; // TODO= best to be a real date obj
-        this.status = ''; // TODO= best to be a real enum obj
-        this.serviceType = ''; // TODO= best to be a real enum obj
-        this.hirePallets = 0;
-        this.nonHirePallets = 0;
-        this.lifts = 0;
-        this.hireRetn = 0;
-        this.goodsType = 0;
-        this.reqTempDeg = 0;
-        this.actualTempDeg = 0;
-        this.unloadFee = 0;
-        this.noChg = false;
-        this.noDisp = false;
-        this.reqPickupDateTime = ''; // TODO= best to be a real date obj
-        this.actPickupDateTime = ''; // TODO= best to be a real date obj
-        this.reqDeliveryTime = '';
-        this.actDeliveryTime = '';
-        this.deliveredTo = '';
-        this.hasPOD = false;
-        this.comments = ''; // TODO= set limit 255
-        this.nextDepot = ''; // TODO= load from db
-        this.deliveryDepot = ''; // TODO= load from db
-        this.currentDepot = ''; // TODO: load from db
-        this.shipPayer = '';
+    public pAKUPKTZ?: string,
 
-        this.userId = 0;
-        this.companyId = 0;
-        this.parentCompanyId = 0;
+    // from depo
+    public pickupFrom?: string,
+    // Seems like general area locations
+    public from?: string,
+    public to?: string,
 
+    public vehicle?: string,
+    public lastVehicle?: string,
+
+    // Prev id number, perhaps
+    public shift?: string,
+
+    public deliveredToName?: string,
+    
+    public isCharged?: boolean,
+    public isOverride?: boolean,
+
+    public invoiceId?: number,
+
+    public hasPOD?: boolean,
+    public comments?: string,
+
+    public pickupDepot?: string,
+    public currentDepot?: string,
+    public nextDepot?: string,
+    public deliveryDepot?: string,
+
+    public currentLocation?: string,
+    public nextLocation?: string,
+
+    // Added SQL 1 to Many Fields
+    public dispatchDocs?: any[],
+    public dispatchHistories?: any[],
+    public dispatchItemLines?: any[],
+    public dispatchMoves?: any[],
+    public dispatchNotes?: any[],
+    public dispatchProofs?: any[]
+) {}
+
+    init(args: any) {
         // Load in kwargs here
         (Object as any).assign(this, args);
     }
+
 }
