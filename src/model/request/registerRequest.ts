bddc8d747ab53e347dc55df8f1e7891ff2f10b18
WhatIsOO <chownema@gmail.com>
2020-05-17 08:48:16 +1200

    FIX: added missing param to req obj

--- src/model/request/registerRequest.ts
+++ src/model/request/registerRequest.ts
@@ -2,6 +2,8 @@ export class RegisterRequest {
     constructor(
         public username?: string,
         public password?: string,
+        public firstName?: string,
+        public lastName?: string,
         public companyName?: string,
         public companyMainEmail?: string,
         public companyQuoteEmail?: string,
