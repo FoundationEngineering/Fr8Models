0cf94ac96acc0b5b9f8857907a93e4fdf9f8ece4
WhatIsOO <chownema@gmail.com>
2020-05-29 08:51:10 +1200

    FEAT: added data to quote interface response

--- src/model/domain/quote/quoteInterface/quoteInterfaceResponse.ts
+++ src/model/domain/quote/quoteInterface/quoteInterfaceResponse.ts
@@ -1,4 +1,4 @@
 export class QuoteInterfaceResponse {
-    constructor(public success: boolean, public cost: number, public message?: any) {}
+    constructor(public success: boolean, public cost: number, public message?: any,  public data?: any) {}
 }
 
