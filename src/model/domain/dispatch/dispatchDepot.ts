efc19001d922a91af8a6dfaa312f63e8f3016754
miggle <miggle@pop-os.localdomain>
2020-06-09 20:04:41 +1200

    FIX: added msising addresslist in depot model

--- src/model/domain/dispatch/dispatchDepot.ts
+++ src/model/domain/dispatch/dispatchDepot.ts
@@ -1,8 +1,11 @@
 import { TemplateDto } from "../templateDto";
+import { PlaceLocationAddress } from "../placeLocationAddress";
 
 export class DispatchDepot extends TemplateDto {
     constructor(
-        public name?: number) {
+        public name?: number,
+        public addressList?: PlaceLocationAddress[]
+    ) {
         super();
     }
 }
