fbcb9b731e562dc9a36567e63e7bf4989315202e
miggle <miggle@pop-os.localdomain>
2021-03-22 19:17:47 +1300

    FIX: made daycode an optional parameter for sign in / out record models

--- src/model/domain/timeSheet/signInRecord.ts
+++ src/model/domain/timeSheet/signInRecord.ts
@@ -2,7 +2,7 @@ import { TemplateDto } from '../templateDto';
 
 export class SignInRecord extends TemplateDto {
     constructor(
-        public dayCode: string,
+        public dayCode?: string,
         public timeRecordId?: number,
         public timeIn?: Date,
     ) {
--- src/model/domain/timeSheet/signOutRecord.ts
+++ src/model/domain/timeSheet/signOutRecord.ts
@@ -2,7 +2,7 @@ import { TemplateDto } from '../templateDto';
 
 export class SignOutRecord extends TemplateDto {
     constructor(
-        public dayCode: string,
+        public dayCode?: string,
         public timeRecordId?: number,
         public timeOut?: Date,
     ) {
